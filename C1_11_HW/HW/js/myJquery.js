d86478642d14b72b0db90e245a6785b2020d6d55
gandzytm <gandzy1984@gmail.com>
2020-07-12 21:22:14 +0500

    delete myJquery

--- C1_11_HW/HW/js/myJquery.js
+++
@@ -1,9 +0,0 @@
-function myJQuery(element) {
-    this.element = element;
-    this.on = function (eventname, f) {
-        for (var i = 0, i < this.element.length; i++) {
-            this.element[i].addEventListener(eventname, f)
-        }
-        return this;
-    }
-}
