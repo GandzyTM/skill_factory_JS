ca8b7030c8e87c10b28803f42614a9f074708c7b
gandzytm <gandzy1984@gmail.com>
2020-07-27 14:21:15 +0500

    HW C3_9 fix

--- C3_9_HW/HW/js/cookies.js
+++ C3_9_HW/HW/js/cookies.js
@@ -1,43 +1,63 @@
-console.log("script added");
+if (getCookie('city')) {
+  console.log('block undefined')
+  let inp = document.getElementById('input-city');
+  inp.remove();
 
-const input_city = document.getElementById('input-city');
-const btn_save = document.querySelector('.btn-save');
+  let p = document.createElement('p');
+  p.innerHTML = 'Ваш город — ' + getCookie('city');
+  document.body.append(p);
 
-function getCookie(name) {
-    let matches = document.cookie.match(new RegExp(
-      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
+  let btnRem = document.createElement('button');
+  btnRem.innerHTML = 'Удалить город';
+  document.body.append(btnRem);
+
+  btnRem.addEventListener('click', async _ => {
+    deleteCookie('city');
+  });
+} else {
+  console.log('block defined')
+
+  let inp = document.getElementById('input-city');
+  inp.oninput = () => {
+    setCookie('city', inp.value, {
+      'max-age': 3600 // cookie live 10 min
+    })
+  };
 }
 
-input_city.placeholder = getCookie('city');
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
 
 function setCookie(name, value, options = {}) {
 
-    options = {
-      path: '/',
-      ...options
-    };
-  
-    if (options.expires.toUTCString) {
-      options.expires = options.expires.toUTCString();
-    }
-  
-    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
-  
-    for (let optionKey in options) {
-      updatedCookie += "; " + optionKey;
-      let optionValue = options[optionKey];
-      if (optionValue !== true) {
-        updatedCookie += "=" + optionValue;
-      }
-    }
-  
-    document.cookie = updatedCookie;
+  options = {
+    path: '/',
+    ...options
+  };
+
+  if (options.expires) {
+    options.expires = options.expires.toUTCString();
   }
 
-btn_save.addEventListener('click', () => {
-    console.log('press btn-save');
-    setCookie('city', 'Vladivostok', {expires: 300});
-});
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
 
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
+
+function deleteCookie(name) {
+  setCookie(name, "", {
+    'max-age': -1
+  })
+} 
